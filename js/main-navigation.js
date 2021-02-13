bb5382c909c7a6ac2a85516f421633b554ed12a2
turbobaguettes <77245337+turbobaguettes@users.noreply.github.com>
2021-02-13 15:38:03 +0100

    Initial commit

---
+++ js/main-navigation.js
@@ -0,0 +1,11 @@
+(() => {
+    const button = document.querySelector(".js-main-navigation__mobile-button");
+    const menu = document.querySelector(".js-main-navigation__menu")
+
+    const toggleClass = () => {
+        menu.classList.toggle("main-navigation__menu--open");
+    };
+
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+})();
